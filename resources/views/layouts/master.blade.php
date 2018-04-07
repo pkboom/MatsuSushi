767193aa290061fb0d8201826f387b66148ccf89
pkboom <ppotpo@gmail.com>
2018-04-07 13:29:44 -0400

    Change favicon

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,40 +1,41 @@
  <!DOCTYPE html>
  <html lang="en">
  <head>
- 	<meta charset="utf-8">
- 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
- 	<meta name="csrf-token" content="{{ csrf_token() }}">
-
- 	<title>{{ config('app.name', 'MatsuSushi') }}</title>
-
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="csrf-token" content="{{ csrf_token() }}">
+	<link rel="shortcut icon" type="image/x-icon" href="/images/sushi.ico" />
+	
+	<title>{{ config('app.name', 'MatsuSushi') }}</title>
+	
 	<link href="{{ asset('css/main.css') }}" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600" rel="stylesheet">
-
-    <style media="screen">
+	
+	<style media="screen">
 		.font-sans {
 			font-family: 'Source Sans Pro', apple-system, BlinkMacSystemFont, 'Helvetica Neue', arial, sans-serif;
 		}
-    </style>
-
+	</style>
+	
 	@yield('header')
-	 
- </head>
- <body class="font-sans text-black">
+	
+</head>
+<body class="font-sans text-black">
 	<div id="app">
-
+		
 		@include('layouts.nav')
-
+		
 		@yield('content') 	
-
+		
 		@include('layouts.footer')
 		
 		<flash message="{{ session('flash') }}"></flash>
-
+		
 		{{--  <chat></chat>  --}}
 	</div>
 	
 	<script src="{{ asset('js/app.js') }}"></script> 
 	
 	@yield('javascript')
- </body>
- </html>
+</body>
+</html>
--- resources/views/menu/menu.blade.php
+++ resources/views/menu/menu.blade.php
@@ -2,18 +2,18 @@
 
 @section('content')
     <div class="w-4/5 mx-auto">
-        <img src="/storage/menu/menu1.jpg" alt="">
-        <img src="/storage/menu/menu2.jpg" alt="">
-        <img src="/storage/menu/menu3.jpg" alt="">
-        <img src="/storage/menu/menu4.jpg" alt="">
-        <img src="/storage/menu/menu5.jpg" alt="">
-        <img src="/storage/menu/menu6.jpg" alt="">
-        <img src="/storage/menu/menu7.jpg" alt="">
-        <img src="/storage/menu/menu8.jpg" alt="">
-        <img src="/storage/menu/menu9.jpg" alt="">
-        <img src="/storage/menu/menu10.jpg" alt="">
-        <img src="/storage/menu/menu11.jpg" alt="">
-        <img src="/storage/menu/menu12.jpg" alt="">
+        <img src="/images/menu/menu1.jpg" alt="">
+        <img src="/images/menu/menu2.jpg" alt="">
+        <img src="/images/menu/menu3.jpg" alt="">
+        <img src="/images/menu/menu4.jpg" alt="">
+        <img src="/images/menu/menu5.jpg" alt="">
+        <img src="/images/menu/menu6.jpg" alt="">
+        <img src="/images/menu/menu7.jpg" alt="">
+        <img src="/images/menu/menu8.jpg" alt="">
+        <img src="/images/menu/menu9.jpg" alt="">
+        <img src="/images/menu/menu10.jpg" alt="">
+        <img src="/images/menu/menu11.jpg" alt="">
+        <img src="/images/menu/menu12.jpg" alt="">
     </div>
 	{{--  <matsu-menu></matsu-menu>  --}}
 @endsection
