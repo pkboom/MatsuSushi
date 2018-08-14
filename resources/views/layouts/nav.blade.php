fe6ac26600aa0d51d2676ecf7f359823e885f48d
pkboom <ppotpo@gmail.com>
2018-08-14 17:37:55 -0400

    make hamburger absolute

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,6 +1,5 @@
 <header id="header" class="absolute bg-transparent sm:bg-white pin-t pin-l w-full py-4 z-10">
-	<div class="flex justify-between px-8">
-		<div class="w-full flex flex-col items-center sm:flex-row sm:justify-between">
+		<div class="w-full flex flex-col items-center sm:flex-row sm:justify-between px-12">
 			<a href="/" class="text-2xl tracking-tight text-grey-darkest font-semibold mb-2">Matsu Sushi</a>
 			<div id="menu" class="hidden flex-col items-center sm:flex sm:flex-row mt-2">
 				<a href="/" class="text-lg leading-normal text-indigo hover:text-indigo-darker opacity-75 no-underline sm:ml-8 mb-2 {{ request()->is('/') ? 'active' : '' }}">HOME</a>
@@ -9,11 +8,10 @@
 				<a href="/contact" class="text-lg leading-normal text-indigo hover:text-indigo-darker opacity-75 no-underline sm:ml-8 mb-2 {{ request()->is('contact') ? 'active' : '' }}">CONTACT</a>
 			</div>
 		</div>
-		<div class="sm:hidden">
+		<div class="sm:hidden absolute pin-t pin-r pr-5 pt-5">
 			<a href="#" class="text-grey-darkest" onclick="
 				document.getElementById('header').classList.toggle('bg-grey-lightest');
 				document.getElementById('menu').classList.toggle('flex');
 			">&#9776;</a>
 		</div>
-	</div>
 </header>
