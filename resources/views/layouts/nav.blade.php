53a7f8e789ac692a53a0ee6f880f3c06e5d7b95e
pkboom <ppotpo@gmail.com>
2018-08-16 16:42:52 -0400

    Prettify moblie menu

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,11 +1,11 @@
 <header id="header" class="absolute bg-transparent sm:bg-white pin-t pin-l w-full py-4 z-10">
 		<div class="w-full flex flex-col items-center sm:flex-row sm:justify-between px-12">
 			<a href="/" class="text-2xl tracking-tight text-grey-darkest font-semibold mb-2">Matsu Sushi</a>
-			<div id="menu" class="hidden flex-col items-center sm:flex sm:flex-row mt-2">
-				<a href="/" class="text-lg leading-normal text-indigo hover:text-indigo-darker opacity-75 no-underline sm:ml-8 mb-2 {{ request()->is('/') ? 'active' : '' }}">HOME</a>
-				<a href="/menu" class="text-lg leading-normal text-indigo hover:text-indigo-darker opacity-75 no-underline sm:ml-8 mb-2 {{ request()->is('menu') ? 'active' : '' }}">MENU</a>
-				<a href="/gallery" class="text-lg leading-normal text-indigo hover:text-indigo-darker opacity-75 no-underline sm:ml-8 mb-2 {{ request()->is('gallery') ? 'active' : '' }}">GALLERY</a>
-				<a href="/contact" class="text-lg leading-normal text-indigo hover:text-indigo-darker opacity-75 no-underline sm:ml-8 mb-2 {{ request()->is('contact') ? 'active' : '' }}">CONTACT</a>
+			<div id="menu" class="w-full hidden flex-col items-center sm:flex sm:flex-row mt-2">
+				<a href="/" class="w-full text-center text-lg leading-normal text-indigo hover:text-indigo-darker opacity-75 no-underline sm:ml-8 py-2 {{ request()->is('/') ? 'active' : '' }}">HOME</a>
+				<a href="/menu" class="w-full text-center text-lg leading-normal text-indigo hover:text-indigo-darker opacity-75 no-underline sm:ml-8 py-2 {{ request()->is('menu') ? 'active' : '' }}">MENU</a>
+				<a href="/gallery" class="w-full text-center text-lg leading-normal text-indigo hover:text-indigo-darker opacity-75 no-underline sm:ml-8 py-2 {{ request()->is('gallery') ? 'active' : '' }}">GALLERY</a>
+				<a href="/contact" class="w-full text-center text-lg leading-normal text-indigo hover:text-indigo-darker opacity-75 no-underline sm:ml-8 py-2 {{ request()->is('contact') ? 'active' : '' }}">CONTACT</a>
 			</div>
 		</div>
 		<div class="sm:hidden absolute pin-t pin-r pr-5 pt-6">
