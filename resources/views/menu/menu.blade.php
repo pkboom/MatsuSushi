5f9e98dee8c03b1bf8ebafd46d32b3e527855889
pkboom <ppotpo@gmail.com>
2018-08-16 16:30:16 -0400

    minus margin-x for menu images

--- resources/views/menu/menu.blade.php
+++ resources/views/menu/menu.blade.php
@@ -2,18 +2,20 @@
 
 @section('content')
     <div class="w-full max-w-xl mx-auto mt-4">
-        <img src="/images/menu/menu1.jpg" alt="">
-        <img src="/images/menu/menu2.jpg" alt="">
-        <img src="/images/menu/menu3.jpg" alt="">
-        <img src="/images/menu/menu4.jpg" alt="">
-        <img src="/images/menu/menu5.jpg" alt="">
-        <img src="/images/menu/menu6.jpg" alt="">
-        <img src="/images/menu/menu7.jpg" alt="">
-        <img src="/images/menu/menu8.jpg" alt="">
-        <img src="/images/menu/menu9.jpg" alt="">
-        <img src="/images/menu/menu10.jpg" alt="">
-        <img src="/images/menu/menu11.jpg" alt="">
-        <img src="/images/menu/menu12.jpg" alt="">
+        <div class="-mx-2 sm:mx-0">
+            <img src="/images/menu/menu1.jpg" alt="">
+            <img src="/images/menu/menu2.jpg" alt="">
+            <img src="/images/menu/menu3.jpg" alt="">
+            <img src="/images/menu/menu4.jpg" alt="">
+            <img src="/images/menu/menu5.jpg" alt="">
+            <img src="/images/menu/menu6.jpg" alt="">
+            <img src="/images/menu/menu7.jpg" alt="">
+            <img src="/images/menu/menu8.jpg" alt="">
+            <img src="/images/menu/menu9.jpg" alt="">
+            <img src="/images/menu/menu10.jpg" alt="">
+            <img src="/images/menu/menu11.jpg" alt="">
+            <img src="/images/menu/menu12.jpg" alt="">
+        </div>
     </div>
 	{{--  <matsu-menu></matsu-menu>  --}}
 @endsection
