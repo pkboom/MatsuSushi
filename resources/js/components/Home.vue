86ba687843e3826000baed1b2248f91501532c32
Keunbae Park <ppotpo@gmail.com>
2020-07-18 14:59:35 -0400

    wip

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -21,7 +21,7 @@
 
 <style scoped>
 #top {
-  background: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),
+  background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
     url('/images/top.png');
   background-size: cover;
   height: 100vh;
