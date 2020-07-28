fdedbda04c1c15aa164bf1ced06fb0877fd5d369
Keunbae Park <ppotpo@gmail.com>
2020-07-27 20:57:54 -0400

    responsive menubook

--- resources/js/components/MenuBook.vue
+++ resources/js/components/MenuBook.vue
@@ -1,16 +1,76 @@
 <template>
   <front-layout title="Menu">
     <div class="max-w-5xl mx-auto min-h-screen">
-      <img src="/images/menu/menu-v1-01.jpg" alt="" />
-      <img src="/images/menu/menu-v1-02.jpg" alt="" />
-      <img src="/images/menu/menu-v1-03.jpg" alt="" />
-      <img src="/images/menu/menu-v1-04.jpg" alt="" />
-      <img src="/images/menu/menu-v1-05.jpg" alt="" />
-      <img src="/images/menu/menu-v1-06.jpg" alt="" />
-      <img src="/images/menu/menu-v1-07.jpg" alt="" />
-      <img src="/images/menu/menu-v1-08.jpg" alt="" />
-      <img src="/images/menu/menu-v1-09.jpg" alt="" />
-      <img src="/images/menu/menu-v1-10.jpg" alt="" />
+      <img
+        srcset="
+          /images/menu/menu-v1-01-1x.jpg 1x,
+          /images/menu/menu-v1-01-2x.jpg 2x
+        "
+        src="/images/menu/menu-v1-01-1x.jpg"
+      />
+      <img
+        srcset="
+          /images/menu/menu-v1-02-1x.jpg 1x,
+          /images/menu/menu-v1-02-2x.jpg 2x
+        "
+        src="/images/menu/menu-v1-02-1x.jpg"
+      />
+      <img
+        srcset="
+          /images/menu/menu-v1-03-1x.jpg 1x,
+          /images/menu/menu-v1-03-2x.jpg 2x
+        "
+        src="/images/menu/menu-v1-03-1x.jpg"
+      />
+      <img
+        srcset="
+          /images/menu/menu-v1-04-1x.jpg 1x,
+          /images/menu/menu-v1-04-2x.jpg 2x
+        "
+        src="/images/menu/menu-v1-04-1x.jpg"
+      />
+      <img
+        srcset="
+          /images/menu/menu-v1-05-1x.jpg 1x,
+          /images/menu/menu-v1-05-2x.jpg 2x
+        "
+        src="/images/menu/menu-v1-05-1x.jpg"
+      />
+      <img
+        srcset="
+          /images/menu/menu-v1-06-1x.jpg 1x,
+          /images/menu/menu-v1-06-2x.jpg 2x
+        "
+        src="/images/menu/menu-v1-06-1x.jpg"
+      />
+      <img
+        srcset="
+          /images/menu/menu-v1-07-1x.jpg 1x,
+          /images/menu/menu-v1-07-2x.jpg 2x
+        "
+        src="/images/menu/menu-v1-07-1x.jpg"
+      />
+      <img
+        srcset="
+          /images/menu/menu-v1-08-1x.jpg 1x,
+          /images/menu/menu-v1-08-2x.jpg 2x
+        "
+        src="/images/menu/menu-v1-08-1x.jpg"
+      />
+      <img
+        srcset="
+          /images/menu/menu-v1-09-1x.jpg 1x,
+          /images/menu/menu-v1-09-2x.jpg 2x
+        "
+        src="/images/menu/menu-v1-09-1x.jpg"
+      />
+      <img
+        srcset="
+          /images/menu/menu-v1-10-1x.jpg 1x,
+          /images/menu/menu-v1-10-2x.jpg 2x
+        "
+        src="/images/menu/menu-v1-10-1x.jpg"
+      />
     </div>
   </front-layout>
 </template>
