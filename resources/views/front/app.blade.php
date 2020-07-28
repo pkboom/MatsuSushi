aaaa4e9fecb60b1166b41da1c382f2a7171011a0
Keunbae Park <ppotpo@gmail.com>
2020-07-27 23:56:52 -0400

    favicon

--- resources/views/front/app.blade.php
+++ resources/views/front/app.blade.php
@@ -14,9 +14,12 @@
     
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-	<link rel="shortcut icon" type="image/x-icon" href="/images/sushi.ico" />
     <script src="https://polyfill.io/v3/polyfill.min.js?features=default" defer></script>
 
     <script>
