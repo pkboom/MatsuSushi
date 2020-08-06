72130f77403e46c259fceef82e1402173dbf7a3b
Keunbae Park <ppotpo@gmail.com>
2020-08-06 19:37:25 -0400

    optimize menubook

--- public/js/admin-app.js
+++ public/js/admin-app.js
@@ -62249,122 +62249,40 @@ var render = function() {
   var _c = _vm._self._c || _h
   return _c("front-layout", { attrs: { title: "Menu" } }, [
     _c("div", { staticClass: "max-w-5xl mx-auto min-h-screen" }, [
+      _c("img", { attrs: { src: "/images/menu/menu-v1-01.jpg" } }),
+      _vm._v(" "),
+      _c("img", { attrs: { src: "/images/menu/menu-v1-02.jpg" } }),
+      _vm._v(" "),
       _c("img", {
-        attrs: {
-          srcset:
-            "/images/menu/menu-v1-01-1x.jpg" +
-            " 1x, " +
-            "/images/menu/menu-v1-01-2x.jpg" +
-            " 2x",
-          src: "/images/menu/menu-v1-01-1x.jpg"
-        }
+        attrs: { loading: "lazy", src: "/images/menu/menu-v1-03.jpg" }
       }),
       _vm._v(" "),
       _c("img", {
-        attrs: {
-          srcset:
-            "/images/menu/menu-v1-02-1x.jpg" +
-            " 1x, " +
-            "/images/menu/menu-v1-02-2x.jpg" +
-            " 2x",
-          src: "/images/menu/menu-v1-02-1x.jpg"
-        }
+        attrs: { loading: "lazy", src: "/images/menu/menu-v1-04.jpg" }
       }),
       _vm._v(" "),
       _c("img", {
-        attrs: {
-          loading: "lazy",
-          srcset:
-            "/images/menu/menu-v1-03-1x.jpg" +
-            " 1x, " +
-            "/images/menu/menu-v1-03-2x.jpg" +
-            " 2x",
-          src: "/images/menu/menu-v1-03-1x.jpg"
-        }
+        attrs: { loading: "lazy", src: "/images/menu/menu-v1-05.jpg" }
       }),
       _vm._v(" "),
       _c("img", {
-        attrs: {
-          loading: "lazy",
-          srcset:
-            "/images/menu/menu-v1-04-1x.jpg" +
-            " 1x, " +
-            "/images/menu/menu-v1-04-2x.jpg" +
-            " 2x",
-          src: "/images/menu/menu-v1-04-1x.jpg"
-        }
+        attrs: { loading: "lazy", src: "/images/menu/menu-v1-06.jpg" }
       }),
       _vm._v(" "),
       _c("img", {
-        attrs: {
-          loading: "lazy",
-          srcset:
-            "/images/menu/menu-v1-05-1x.jpg" +
-            " 1x, " +
-            "/images/menu/menu-v1-05-2x.jpg" +
-            " 2x",
-          src: "/images/menu/menu-v1-05-1x.jpg"
-        }
+        attrs: { loading: "lazy", src: "/images/menu/menu-v1-07.jpg" }
       }),
       _vm._v(" "),
       _c("img", {
-        attrs: {
-          loading: "lazy",
-          srcset:
-            "/images/menu/menu-v1-06-1x.jpg" +
-            " 1x, " +
-            "/images/menu/menu-v1-06-2x.jpg" +
-            " 2x",
-          src: "/images/menu/menu-v1-06-1x.jpg"
-        }
+        attrs: { loading: "lazy", src: "/images/menu/menu-v1-08.jpg" }
       }),
       _vm._v(" "),
       _c("img", {
-        attrs: {
-          loading: "lazy",
-          srcset:
-            "/images/menu/menu-v1-07-1x.jpg" +
-            " 1x, " +
-            "/images/menu/menu-v1-07-2x.jpg" +
-            " 2x",
-          src: "/images/menu/menu-v1-07-1x.jpg"
-        }
+        attrs: { loading: "lazy", src: "/images/menu/menu-v1-09.jpg" }
       }),
       _vm._v(" "),
       _c("img", {
-        attrs: {
-          loading: "lazy",
-          srcset:
-            "/images/menu/menu-v1-08-1x.jpg" +
-            " 1x, " +
-            "/images/menu/menu-v1-08-2x.jpg" +
-            " 2x",
-          src: "/images/menu/menu-v1-08-1x.jpg"
-        }
-      }),
-      _vm._v(" "),
-      _c("img", {
-        attrs: {
-          loading: "lazy",
-          srcset:
-            "/images/menu/menu-v1-09-1x.jpg" +
-            " 1x, " +
-            "/images/menu/menu-v1-09-2x.jpg" +
-            " 2x",
-          src: "/images/menu/menu-v1-09-1x.jpg"
-        }
-      }),
-      _vm._v(" "),
-      _c("img", {
-        attrs: {
-          loading: "lazy",
-          srcset:
-            "/images/menu/menu-v1-10-1x.jpg" +
-            " 1x, " +
-            "/images/menu/menu-v1-10-2x.jpg" +
-            " 2x",
-          src: "/images/menu/menu-v1-10-1x.jpg"
-        }
+        attrs: { loading: "lazy", src: "/images/menu/menu-v1-10.jpg" }
       })
     ])
   ])
--- resources/js/components/Menubook.vue
+++ resources/js/components/Menubook.vue
@@ -1,84 +1,16 @@
 <template>
   <front-layout title="Menu">
     <div class="max-w-5xl mx-auto min-h-screen">
-      <img
-        srcset="
-          /images/menu/menu-v1-01-1x.jpg 1x,
-          /images/menu/menu-v1-01-2x.jpg 2x
-        "
-        src="/images/menu/menu-v1-01-1x.jpg"
-      />
-      <img
-        srcset="
-          /images/menu/menu-v1-02-1x.jpg 1x,
-          /images/menu/menu-v1-02-2x.jpg 2x
-        "
-        src="/images/menu/menu-v1-02-1x.jpg"
-      />
-      <img
-        loading="lazy"
-        srcset="
-          /images/menu/menu-v1-03-1x.jpg 1x,
-          /images/menu/menu-v1-03-2x.jpg 2x
-        "
-        src="/images/menu/menu-v1-03-1x.jpg"
-      />
-      <img
-        loading="lazy"
-        srcset="
-          /images/menu/menu-v1-04-1x.jpg 1x,
-          /images/menu/menu-v1-04-2x.jpg 2x
-        "
-        src="/images/menu/menu-v1-04-1x.jpg"
-      />
-      <img
-        loading="lazy"
-        srcset="
-          /images/menu/menu-v1-05-1x.jpg 1x,
-          /images/menu/menu-v1-05-2x.jpg 2x
-        "
-        src="/images/menu/menu-v1-05-1x.jpg"
-      />
-      <img
-        loading="lazy"
-        srcset="
-          /images/menu/menu-v1-06-1x.jpg 1x,
-          /images/menu/menu-v1-06-2x.jpg 2x
-        "
-        src="/images/menu/menu-v1-06-1x.jpg"
-      />
-      <img
-        loading="lazy"
-        srcset="
-          /images/menu/menu-v1-07-1x.jpg 1x,
-          /images/menu/menu-v1-07-2x.jpg 2x
-        "
-        src="/images/menu/menu-v1-07-1x.jpg"
-      />
-      <img
-        loading="lazy"
-        srcset="
-          /images/menu/menu-v1-08-1x.jpg 1x,
-          /images/menu/menu-v1-08-2x.jpg 2x
-        "
-        src="/images/menu/menu-v1-08-1x.jpg"
-      />
-      <img
-        loading="lazy"
-        srcset="
-          /images/menu/menu-v1-09-1x.jpg 1x,
-          /images/menu/menu-v1-09-2x.jpg 2x
-        "
-        src="/images/menu/menu-v1-09-1x.jpg"
-      />
-      <img
-        loading="lazy"
-        srcset="
-          /images/menu/menu-v1-10-1x.jpg 1x,
-          /images/menu/menu-v1-10-2x.jpg 2x
-        "
-        src="/images/menu/menu-v1-10-1x.jpg"
-      />
+      <img src="/images/menu/menu-v1-01.jpg" />
+      <img src="/images/menu/menu-v1-02.jpg" />
+      <img loading="lazy" src="/images/menu/menu-v1-03.jpg" />
+      <img loading="lazy" src="/images/menu/menu-v1-04.jpg" />
+      <img loading="lazy" src="/images/menu/menu-v1-05.jpg" />
+      <img loading="lazy" src="/images/menu/menu-v1-06.jpg" />
+      <img loading="lazy" src="/images/menu/menu-v1-07.jpg" />
+      <img loading="lazy" src="/images/menu/menu-v1-08.jpg" />
+      <img loading="lazy" src="/images/menu/menu-v1-09.jpg" />
+      <img loading="lazy" src="/images/menu/menu-v1-10.jpg" />
     </div>
   </front-layout>
 </template>
