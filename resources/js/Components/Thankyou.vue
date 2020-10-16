a3dac41d69b4f4c48760f34f5799a44602c3af21
Keunbae Park <ppotpo@gmail.com>
2020-10-16 07:39:47 -0400

    remove personal info in thankyou

--- resources/js/Components/Thankyou.vue
+++ resources/js/Components/Thankyou.vue
@@ -20,46 +20,6 @@
           <span class="text-gray-500">Order number:</span>
           {{ transaction.id }}
         </div>
-        <div>
-          <span class="text-gray-500">Name:</span>
-          {{ transaction.name }}
-        </div>
-        <div>
-          <span class="text-gray-500">Phone:</span>
-          {{ transaction.phone }}
-        </div>
-        <div v-if="transaction.takeout_time">
-          <span class="text-gray-500">Takeout time:</span>
-          {{ transaction.takeout_time }}
-        </div>
-        <div v-if="transaction.address">
-          <span class="text-gray-500">Address:</span>
-          {{ transaction.address }}
-        </div>
-        <div>
-          <span class="text-gray-500">Subtotal:</span>
-          $ {{ transaction.subtotal }}
-        </div>
-        <div>
-          <span class="text-gray-500">Tax:</span>
-          $ {{ transaction.tax }}
-        </div>
-        <div>
-          <span class="text-gray-500">Tip:</span>
-          $ {{ transaction.tip }}
-        </div>
-        <div v-if="transaction.type === 'delivery'">
-          <span class="text-gray-500">Delivery fee:</span>
-          $ {{ transaction.fee }}
-        </div>
-        <div>
-          <span class="text-gray-500">Total:</span>
-          $ {{ transaction.total }}
-        </div>
-        <div v-if="transaction.message">
-          <span class="text-gray-500">Message:</span>
-          {{ transaction.message }}
-        </div>
         <div><span class="text-gray-500">Items:</span></div>
         <div class="space-y-3">
           <div
