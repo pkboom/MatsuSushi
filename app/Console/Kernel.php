e51626eb53bab80da7175e71768492353ce36434
Keunbae Park <ppotpo@gmail.com>
2020-11-29 04:39:43 -0500

    wip

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command(ConfirmOldTransactions::class)
-            ->everyTenMinutes();
+            ->everyMinute();
+        // ->everyTenMinutes();
     }
 
     /**
