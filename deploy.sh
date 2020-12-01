a76f49abeeb9a3a5c2362d81c3ff45cee5d1c033
Keunbae Park <ppotpo@gmail.com>
2020-11-30 22:51:29 -0500

    wip

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ cd /var/www/html
 git pull origin master
 composer install --no-interaction --prefer-dist --optimize-autoloader
 
-sudo -S service php-fpm7.4 reload 
+sudo -S service php7.4-fpm reload 
 
 if [ -f artisan ]; then
     php artisan migrate --force
