2d72bc45fefe516427f034452b33f39a9b6d3e4f
Keunbae Park <ppotpo@gmail.com>
2020-12-28 23:07:04 -0500

    remove promotion from cart page

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Transaction;
-use Illuminate\Support\Facades\Cache;
 use Inertia\Inertia;
 
 class CartController extends Controller
@@ -12,20 +11,6 @@ class CartController extends Controller
     {
         return Inertia::render('Cart', [
             'fee' => Transaction::DELIVERY_FEE,
-            'promotion' => [
-                'over20' => [
-                    'value' => Transaction::PROMOTION_20,
-                    'name' => Cache::get(Transaction::promotionNameOver20()),
-                ],
-                'over50' => [
-                    'value' => Transaction::PROMOTION_50,
-                    'name' => Cache::get(Transaction::promotionNameOver50()),
-                ],
-                'over100' => [
-                    'value' => Transaction::PROMOTION_100,
-                    'name' => Cache::get(Transaction::promotionNameOver100()),
-                ],
-            ],
         ]);
     }
 }
