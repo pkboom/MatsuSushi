a5121b077d1404c9b7279ff463ec09e47c3a1d98
Keunbae Park <ppotpo@gmail.com>
2021-01-02 16:56:38 -0500

    wip

--- resources/js/Pages/Menu.vue
+++ resources/js/Pages/Menu.vue
@@ -1,17 +1,17 @@
 <template>
   <front-layout title="Menu">
     <div class="max-w-5xl mx-auto min-h-screen">
-      <img src="/images/menu/01.svg" width="744" height="1053" />
-      <img src="/images/menu/02.svg" width="744" height="1053" />
-      <img loading="lazy" src="/images/menu/03.svg" width="744" height="1053" />
-      <img loading="lazy" src="/images/menu/04.svg" width="744" height="1053" />
-      <img loading="lazy" src="/images/menu/05.svg" width="744" height="1053" />
-      <img loading="lazy" src="/images/menu/06.svg" width="744" height="1053" />
-      <img loading="lazy" src="/images/menu/07.svg" width="744" height="1053" />
-      <img loading="lazy" src="/images/menu/08.svg" width="744" height="1053" />
-      <img loading="lazy" src="/images/menu/09.svg" width="744" height="1053" />
-      <img loading="lazy" src="/images/menu/10.svg" width="744" height="1053" />
-      <img loading="lazy" src="/images/menu/11.svg" width="744" height="1053" />
+      <img src="/images/menu/01.png" />
+      <img src="/images/menu/02.png" />
+      <img loading="lazy" src="/images/menu/03.png" />
+      <img loading="lazy" src="/images/menu/04.png" />
+      <img loading="lazy" src="/images/menu/05.png" />
+      <img loading="lazy" src="/images/menu/06.png" />
+      <img loading="lazy" src="/images/menu/07.png" />
+      <img loading="lazy" src="/images/menu/08.png" />
+      <img loading="lazy" src="/images/menu/09.png" />
+      <img loading="lazy" src="/images/menu/10.png" />
+      <img loading="lazy" src="/images/menu/11.png" />
     </div>
   </front-layout>
 </template>
