cf2d5d946d97c51e289659e453b870752f5484c9
Keunbae Park <ppotpo@gmail.com>
2021-02-15 15:49:21 -0500

    wip

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
 # run: ssh matsu 'cd /var/www/html && ./deploy.sh'
+ssh -t matsu '
+    cd /var/www/html
 
-git reset --hard && git clean -df
-git pull origin master
-composer install --no-interaction --prefer-dist --optimize-autoloader
+    git reset --hard && git clean -df
+    git pull origin master
+    composer install --no-interaction --prefer-dist --optimize-autoloader
 
-sudo -S service php7.4-fpm reload 
+    sudo -S service php7.4-fpm reload 
 
-if [ -f artisan ]; then
-    php7.4 artisan migrate --force
-fi
+    if [ -f artisan ]; then
+        php7.4 artisan migrate --force
+    fi
 
-php7.4 artisan config:cache
+    php7.4 artisan config:cache
 
-sudo npm ci
+    sudo npm ci
 
-sudo npm run production
+    sudo npm run production 
+'
+
+echo 'Deployed.'
